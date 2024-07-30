ff6f276f5c55ad0a5aac94b9de679af2b6071fc1
Vinayak <netcon@Zias-MacBook-Air.local>
2024-07-30 19:00:28 +0530

    renamed group id

--- copperaiandroidsdklib/build.gradle.kts
+++ copperaiandroidsdklib/build.gradle.kts
@@ -1,6 +1,12 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.vanniktech.maven.publish") version "0.29.0"
+
 }
 
 android {
@@ -30,6 +36,48 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+}
+
+mavenPublishing{
+    configure(AndroidSingleVariantLibrary(
+        variant = "release",
+        sourcesJar = true,
+        publishJavadocJar = true,
+    ))
+
+    coordinates(
+        groupId = "ai.yavar.mavencl",
+        artifactId = "copper-ai-android-sdk",
+        version = "0.0.1"
+    )
+
+    pom{
+        name.set("Copper AI Android SDK")
+        description.set("A Copper AI Agent bot")
+
+        inceptionYear.set("2024")
+        url.set("https://github.com/yavar-ai/copper-ai-android-sdk")
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/license/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("yavar-ai")
+                name.set("Yavar ai")
+                url.set("https://github.com/yavar-ai")
+            }
+        }
+        scm {
+            url.set("https://github.com/yavar-ai/copper-ai-android-sdk")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
 }
 
 dependencies {
